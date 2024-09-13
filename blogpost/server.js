4fd984dcb8ce3edac209d2d11bf770b8fb2d0ba6
unknown <bibhudattarw@gmail.com>
2024-09-13 17:49:53 +0530

    updated code

--- blogpost/server.js
+++ blogpost/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.get('/', (req, res) => {
+app.get('/t', (req, res) => {
     res.send('APIs SERVER is perfectly working..');
 
     res.json({
